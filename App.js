9c79890245cb671a63b0ab10ea18ba3d89e76aad
Juzdalua <bearwcw@naver.com>
2022-02-07 23:42:15 +0900

    Update mobile layout with flex

--- App.js
+++ App.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import {
-  StatusBar,
-  StyleSheet,
-  Text,
   View,
 } from 'react-native';
 
 // div => View, span => Text
+// display is only flex, flexDirection default is Column
+// width, height는 사용하지 않는다. -> 반응형을 위해 flex 사용.
+// 부모 엘리먼트에 따라 비율이 정해진다.
 const App = () => {
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hi</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={{flex:1, backgroundColor:"tomato"}}></View>
+      <View style={{flex:3, backgroundColor:"teal"}}></View>      
+      <View style={{flex:1, backgroundColor:"grey"}}></View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",        
-  },
-  text: {
-    fontSize: 48,
-  }
-});
 
 export default App;
